41487747f98b3a5fbe53d7dbe6308a2e7c5f79cb
Austin Kibler <austin@kibler.dev>
2021-06-17 13:57:38 -0500

    more checking for other sites

--- stockcheck.py
+++ stockcheck.py
@@ -5,20 +5,24 @@ import requests
 import bs4
 
 class StockCheck():
-    def __init__(self, url):
-        self.url = url
+  def __init__(self, url):
+    self.url = url
 
-    def checkStock(self):
-        '''Check if item is in stock.'''
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'}
-        res = requests.get(self.url, headers=headers)
+  def checkStock(self):
+    '''Check if item is in stock.'''
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'}
+    res = requests.get(self.url, headers=headers)
 
-        res.raise_for_status()
-        noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-        buttons = noStarchSoup.select('button')
+    res.raise_for_status()
+    noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+    buttons = noStarchSoup.select('button')
+    divs = noStarchSoup.select('div')
 
-        if 'Sold Out' in str(buttons):
-            return False
-        else:
-            return True
+    if 'sold out' in str(buttons).lower() or 'not available' in str(buttons).lower():
+      return False
+    else:
+      return True
+
+  def getUrl(self):
+    return self.url
