c743a5e56463febd4c2720a4372906a163723d9f
Austin Kibler <austin@kibler.dev>
2021-06-17 14:25:41 -0500

    import from secrets

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 from discord.ext import tasks
 from stockcheck import StockCheck
+from secrets import BOT_KEY, CHANNEL_KEY, ROLE_KEY, TEST_CHANNEL_KEY
 
 import discord
 
@@ -8,8 +9,11 @@ class MyClient(discord.Client):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     # an attribute we can access from our task
-    self.stores = [StockCheck('https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149'), StockCheck('https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161'), StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html'), StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html')]
-    self.role = "<@&855138343095631912>"
+    self.stores = [StockCheck('https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149'), 
+                    StockCheck('https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161'), 
+                    StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html'), 
+                    StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html')]
+    self.role = ROLE_KEY
     # start the task to run in the background
     self.my_background_task.start()
 
@@ -19,7 +23,7 @@ class MyClient(discord.Client):
 
   @tasks.loop(seconds=60) # task runs every 60 seconds
   async def my_background_task(self):
-    channel = self.get_channel(285648978115166209) # channel ID goes here
+    channel = self.get_channel(TEST_CHANNEL_KEY) # channel ID goes here
     instock = False
     for store in self.stores:
       if store.checkStock():
@@ -35,4 +39,4 @@ class MyClient(discord.Client):
 
 
 client = MyClient()
-client.run('Bot Key')
+client.run(BOT_KEY)
