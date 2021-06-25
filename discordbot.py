867d3cef80dc12c49bc1e00e5cc7d7c346b4330b
Austin Kibler <austin@kibler.dev>
2021-06-25 12:02:29 -0500

    support for a gpu

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 from discord.ext import tasks
 from stockcheck import StockCheck
-from secrets import BOT_KEY, CHANNEL_KEY, XBOX_ROLE_KEY, PS4_ROLE_KEY, TEST_CHANNEL_KEY
+from secrets import BOT_KEY, CHANNEL_KEY, XBOX_ROLE_KEY, PS4_ROLE_KEY, GPU_ROLE_KEY, TEST_CHANNEL_KEY
 
 import discord
 
@@ -14,9 +14,11 @@ class MyClient(discord.Client):
                     StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html'), 
                     StockCheck('https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html'),
                     StockCheck('https://www.gamestop.com/video-games/xbox-series-x/consoles/products/xbox-series-x/B224744V.html'),
-                    StockCheck('https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324')]
+                    StockCheck('https://www.gamestop.com/video-games/xbox-series-x/consoles/products/xbox-series-x/B224744V.html'),
+                    StockCheck('https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440')]
     self.psrole = PS4_ROLE_KEY
     self.xbrole = XBOX_ROLE_KEY
+    self.gpurole = GPU_ROLE_KEY
     self.counter = 0
     self.instock = False
     # start the task to run in the background
@@ -29,7 +31,7 @@ class MyClient(discord.Client):
     f.write(f'Logged in as {self.user} (ID: {self.user.id})\n')
     f.close()
 
-  @tasks.loop(seconds=60) # task runs every 60 seconds
+  @tasks.loop(seconds=90) # task runs every 90 seconds
   async def my_background_task(self):
     channel = self.get_channel(CHANNEL_KEY) # channel ID goes here
     instock = False
@@ -40,8 +42,10 @@ class MyClient(discord.Client):
           await channel.send(f"{self.psrole} Item in stock at {store.getUrl()}")
         elif "xbox" in store.getUrl():
           await channel.send(f"{self.xbrole} Item in stock at {store.getUrl()}")
+        elif "nvidia" in store.getUrl():
+          await channel.send(f"{self.gpurole} Item in stock at {store.getUrl()}")
         else:
-          await channel.send(f"{self.xbrole} {self.psrole} Item in stock at {store.getUrl()}")
+          await channel.send(f"{self.xbrole} {self.psrole} {self.gpurole} Item in stock at {store.getUrl()}")
     if self.counter == 1440:
       self.counter = 0
       if not self.instock:
